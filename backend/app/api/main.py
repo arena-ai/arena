03c91e2a41426ac1e83a3cde083363615f6f1535
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-03-27 19:19:40 +0100

    fixed API

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import events, login, users, utils
+from app.api.routes import settings, events, login, users, utils
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(events.router, prefix="/events", tags=["events"])
-api_router.include_router(events.router, prefix="/settings", tags=["settings"])
+api_router.include_router(settings.router, prefix="/settings", tags=["settings"])
