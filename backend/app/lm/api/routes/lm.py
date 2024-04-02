f55102ad82dff92c83139076dbee2855c1372ddd
Nicolas Grislain <nicolas.grislain@gmail.com>
2024-04-03 00:09:41 +0200

    ok

--- backend/app/lm/api/routes/lm.py
+++ backend/app/lm/api/routes/lm.py
@@ -9,6 +9,7 @@ from app.lm.models import ChatCompletion, CreateChatCompletion
 
 from openai import OpenAI
 from mistralai.client import MistralClient
+from anthropic import Anthropic
 
 router = APIRouter()
 
@@ -39,14 +40,14 @@ def mistral_chat_completion(
     return ChatCompletion.model_validate(chat_completion, strict=False, from_attributes=True)
 
 
-@router.post("/antropic/chat/completions", response_model=ChatCompletion)
+@router.post("/anthropic/v1/messages", response_model=ChatCompletion)
 def mistral_chat_completion(
     session: SessionDep, current_user: CurrentUser, chat_completion_in: CreateChatCompletion
 ) -> Any:
     """
-    Mistral integration
+    Anthropic integration
     """
-    mistral_api_key = crud.get_setting(session=session, setting_name="MISTRAL_API_KEY", owner_id=current_user.id)
-    client = MistralClient(api_key=mistral_api_key.content)
-    chat_completion = client.chat(**chat_completion_in.model_dump(exclude_none=True))
+    anthropic_api_key = crud.get_setting(session=session, setting_name="ANTHROPIC_API_KEY", owner_id=current_user.id)
+    client = Anthropic(api_key=anthropic_api_key.content)
+    chat_completion = client.messages.create(**chat_completion_in.model_dump(exclude_none=True))
     return ChatCompletion.model_validate(chat_completion, strict=False, from_attributes=True)
--- backend/app/tests/api/routes/test_lm.py
+++ backend/app/tests/api/routes/test_lm.py
@@ -36,6 +36,7 @@ def test_openai(
     content = response.json()
     assert len(content["choices"]) == 1
 
+
 def test_mistral(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
@@ -47,6 +48,36 @@ def test_mistral(
     )
     response = client.post(
         f"{settings.API_V1_STR}/lm/mistral/chat/completions",
+        headers=superuser_token_headers,
+        json={
+            "model": "mistral-small",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant."
+                },
+                {
+                    "role": "user",
+                    "content": "Who is Victor Hugo? Where does he live?"
+                }
+            ]
+        },
+    )
+    assert response.status_code == 200
+    content = response.json()
+
+
+def test_anthropic(
+    client: TestClient, superuser_token_headers: dict[str, str], db: Session
+) -> None:
+    # Setup a token
+    client.post(
+        f"{settings.API_V1_STR}/settings/",
+        headers=superuser_token_headers,
+        json={"name": "ANTHROPIC_API_KEY", "content": os.getenv("ANTHROPIC_API_KEY")},
+    )
+    response = client.post(
+        f"{settings.API_V1_STR}/lm/anthropic/v1/messages",
         headers=superuser_token_headers,
         json={
             "model": "mistral-small",
