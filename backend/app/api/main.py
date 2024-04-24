5ae894e5b70721a96e1e855932666d1cd2af338e
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-04-24 16:19:49 +0200

    ok

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.routes import settings, events, login, users, utils
-from app.lm.api.routes import lm
+from app.lm.api.routes import chat_completion
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -10,4 +10,4 @@ api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(events.router, prefix="/events", tags=["events"])
 api_router.include_router(settings.router, prefix="/settings", tags=["settings"])
 # Include language_models
-api_router.include_router(lm.router, prefix="/lm", tags=["language-models"])
+api_router.include_router(chat_completion.router, prefix="/lm", tags=["language-models"])
