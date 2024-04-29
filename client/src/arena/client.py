faf8c37ba8860125eb5844ac5c92e89d7f11c30f
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-04-29 11:55:40 +0200

    Working basic client

--- client/src/arena/client.py
+++ client/src/arena/client.py
@@ -1,14 +1,17 @@
+from typing import Any
 import httpx
 from arena.models import ChatCompletionRequest, ChatCompletionResponse
 
 BASE_URL = "https://arena.sarus.app/api/v1"
 
 class Client:
-    def __init__(self, user: str | None, password: str | None, api_key: str | None, base_url: str = BASE_URL):
+    def __init__(self, user: str | None = None, password: str | None = None, api_key: str | None = None, base_url: str = BASE_URL):
         self.user = user
         self.password = password
         self.api_key = api_key
         self.base_url = base_url
+        if not self.api_key:
+            self.login()
     
     def login(self):
         assert(self.user and self.password)
@@ -28,15 +31,17 @@ class Client:
                     "client_secret": "",
                 },
             )
-        self.api_key = resp.json()
+        self.api_key = resp.json()['access_token']
 
-    def chat_completion(self, req: ChatCompletionRequest) -> ChatCompletionResponse:
+    def chat_completions(self, **kwargs: Any) -> ChatCompletionResponse:
+        req = ChatCompletionRequest.model_validate(kwargs).model_dump(mode="json", exclude_unset=True, exclude_none=True)
+        print(f"\nDEBUG {req}")
         with httpx.Client() as client:
             resp = client.post(
-                url = f"{self.base_url}/chat/completion",
+                url = f"{self.base_url}/lm/chat/completions",
                 headers = {
                     "Authorization": f"Bearer {self.api_key}"
                 },
-                content=req,
+                json=req,
             )
         return resp
--- client/tests/test_client.py
+++ client/tests/test_client.py
@@ -8,12 +8,11 @@ def test_chat():
     user = os.getenv("FIRST_SUPERUSER")
     password = os.getenv("FIRST_SUPERUSER_PASSWORD")
     client = Client(user=user, password=password)
-    print(client.login())
-    # client = OpenAI(api_key=api_key)
-    # resp = client.chat.completions.create(model="gpt-3.5-turbo", messages=[
-    #     {"role": "system", "content": "You are a helpful assistant."},
-    #     {"role": "user", "content": "What is the fastest animal on earth?"},
-    # ])
+    resp = client.chat_completions(model="gpt-3.5-turbo", messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the fastest animal on earth?"},
+    ])
+    print(resp)
     # assert(resp.choices[0].message.role == "assistant")
     # print(resp.choices[0].message.content)
     # assert(len(resp.choices[0].message.content) > 10)
