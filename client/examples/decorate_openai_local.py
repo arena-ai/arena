70cda3bc0b75658da13ee18821ead22ce8e5d2dd
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-04-30 09:23:26 +0200

    examples ok

--- client/examples/decorate_openai_local.py
+++ client/examples/decorate_openai_local.py
@@ -1,13 +1,11 @@
 import os
 from openai import OpenAI
-from rich import print
 from dotenv import load_dotenv
 from arena.client import Client
 # Load .env
 load_dotenv()
 
 def simple_chat_completion():
-    print("\n[bold red]Simple OpenAI chat completion")
     api_key = os.getenv("ARENA_OPENAI_API_KEY")
     client = OpenAI(api_key=api_key)
     resp = client.chat.completions.create(model="gpt-3.5-turbo", messages=[
@@ -18,11 +16,10 @@ def simple_chat_completion():
 
 
 def decorated_chat_completion():
-    print("\n[bold red]Decorated OpenAI chat completion")
     # Added this
     user = os.getenv("FIRST_SUPERUSER")
     password = os.getenv("FIRST_SUPERUSER_PASSWORD")
-    arena = Client(user=user, password=password)
+    arena = Client(user=user, password=password, base_url="http://localhost/api/v1")
     arena.decorate(OpenAI)
 
     # Everything is unchanged then
@@ -36,10 +33,9 @@ def decorated_chat_completion():
 
 
 def decorated_chat_completion_with_user_eval():
-    print("\n[bold red]Decorated OpenAI chat completion with eval")
     user = os.getenv("FIRST_SUPERUSER")
     password = os.getenv("FIRST_SUPERUSER_PASSWORD")
-    arena = Client(user=user, password=password)
+    arena = Client(user=user, password=password, base_url="http://localhost/api/v1")
     arena.decorate(OpenAI)
 
     api_key = os.getenv("ARENA_OPENAI_API_KEY")
@@ -53,45 +49,40 @@ def decorated_chat_completion_with_user_eval():
     # Added this
     arena.evaluation(resp.id, 0.98)
 
-def arena_chat_completion_with_eval():
-    print("\n[bold red]Arena chat completion with user eval")
+def arena_chat_completion_with_user_eval():
     user = os.getenv("FIRST_SUPERUSER")
     password = os.getenv("FIRST_SUPERUSER_PASSWORD")
-    arena = Client(user=user, password=password)
-    arena.anthropic_api_key(os.getenv("ARENA_ANTHROPIC_API_KEY"))
+    arena = Client(user=user, password=password, base_url="http://localhost/api/v1")
+    arena.openai_api_key(os.getenv("ARENA_OPENAI_API_KEY"))
 
-    resp = arena.chat_completions(model="claude-2.1", messages=[
+    resp = arena.chat_completions(model="gpt-3.5-turbo", messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "What is the fastest animal on earth?"},
         ],
-        arena_parameters={"judge_evaluation": True},
-        streaming=False,
-        max_tokens=100,
+        arena_parameters={"judge_evaluation": True}
     )
     print(resp.choices[0].message.content)
     # Added this
     arena.evaluation(resp.id, 0.98)
 
-def arena_chat_completion_with_eval_from_test():
-    print("\n[bold red]Arena chat completion with user eval from test")
+def arena_chat_completion_with_test_user_eval():
     user = "test@sarus.tech"
-    password = "password"
-    arena = Client(user=user, password=password)
+    password = "Password1"
+    arena = Client(user=user, password=password, base_url="http://localhost/api/v1")
     arena.openai_api_key(os.getenv("ARENA_OPENAI_API_KEY"))
-    arena.mistral_api_key(os.getenv("ARENA_MISTRAL_API_KEY"))
 
-    resp = arena.chat_completions(model="mistral-small", messages=[
+    resp = arena.chat_completions(model="gpt-3.5-turbo", messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "What are the 10 first even prime numbers?"},
         ],
-        arena_parameters={"judge_evaluation": True},
+        arena_parameters={"judge_evaluation": True}
     )
     print(resp.choices[0].message.content)
     # Added this
     arena.evaluation(resp.id, 0.98)
 
-simple_chat_completion()
-decorated_chat_completion()
-decorated_chat_completion_with_user_eval()
-arena_chat_completion_with_eval()
-arena_chat_completion_with_eval_from_test()
+# simple_chat_completion()
+# decorated_chat_completion()
+# decorated_chat_completion_with_user_eval()
+# arena_chat_completion_with_user_eval()
+arena_chat_completion_with_test_user_eval()
