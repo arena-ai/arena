d4fbbe0add81e42bb92702bc29fa3f7b8cca26c4
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-05-02 16:41:18 +0200

    anon

--- backend/app/services/masking.py
+++ backend/app/services/masking.py
@@ -1,7 +1,7 @@
 from typing import Mapping, Sequence, Literal, Any
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, TypeAdapter
 
 import httpx
 
@@ -23,15 +23,86 @@ class AnalyzerRequest(BaseModel):
     return_decision_process: bool | None = None
 
 
+class AnalyzerResponseItem(BaseModel):
+    entity_type: str
+    start: int
+    end: int
+    score: float
+    analysis_explanation: Mapping[str, Any] | None = None
+    recognition_metadata: Mapping[str, Any] | None = None
+
+
+analyzer_response = TypeAdapter(Sequence[AnalyzerResponseItem])
+
+class Replace(BaseModel):
+    type: Literal["replace"] = "replace"
+    new_value: str
+
+class Redact(BaseModel):
+    type: Literal["redact"] = "redact"
+
+class Mask(BaseModel):
+    type: Literal["mask"] = "mask"
+    masking_char: str
+    chars_to_mask: int
+    from_end: bool = False
+
+class Hash(BaseModel):
+    type: Literal["hash"] = "hash"
+    hash_type: Literal["md5", "sha256", "sha512"] = "md5"
+
+class Encrypt(BaseModel):
+    type: Literal["encrypt"] = "encrypt"
+    key: str
+
+
+class Anonymizers(BaseModel):
+    PHONE_NUMBER: Replace | Redact | Mask | Hash | Encrypt
+    US_DRIVER_LICENSE: Replace | Redact | Mask | Hash | Encrypt
+    US_PASSPORT: Replace | Redact | Mask | Hash | Encrypt
+    LOCATION: Replace | Redact | Mask | Hash | Encrypt
+    CREDIT_CARD: Replace | Redact | Mask | Hash | Encrypt
+    CRYPTO: Replace | Redact | Mask | Hash | Encrypt
+    UK_NHS: Replace | Redact | Mask | Hash | Encrypt
+    US_SSN: Replace | Redact | Mask | Hash | Encrypt
+    US_BANK_NUMBER: Replace | Redact | Mask | Hash | Encrypt
+    EMAIL_ADDRESS: Replace | Redact | Mask | Hash | Encrypt
+    DATE_TIME: Replace | Redact | Mask | Hash | Encrypt
+    IP_ADDRESS: Replace | Redact | Mask | Hash | Encrypt
+    PERSON: Replace | Redact | Mask | Hash | Encrypt
+    IBAN_CODE: Replace | Redact | Mask | Hash | Encrypt
+    NRP: Replace | Redact | Mask | Hash | Encrypt
+    US_ITIN: Replace | Redact | Mask | Hash | Encrypt
+    MEDICAL_LICENSE: Replace | Redact | Mask | Hash | Encrypt
+    URL: Replace | Redact | Mask | Hash | Encrypt
+    DEFAULT: Replace | Redact | Mask | Hash | Encrypt
+
+
+class AnonymizerRequest(BaseModel):
+    text: str
+    anonymizers: Any
+
 
 @dataclass
 class Analyzer:
     url: str = f"http://{settings.PRESIDIO_ANALYZER_SERVER}:{settings.PRESIDIO_ANALYZER_PORT}/analyze"
 
-    async def analyze(self, req: AnalyzerRequest) -> str:
+    async def analyze(self, req: AnalyzerRequest) -> Sequence[AnalyzerResponseItem]:
         async with httpx.AsyncClient() as client:
             response = await client.post(
                 url=f"{self.url}",
                 json=req.model_dump(exclude_none=True),
             )
-            return response.raise_for_status().json()
+            return analyzer_response.validate_python(response.raise_for_status().json())
+
+@dataclass
+class Anonymizer:
+    url: str = f"http://{settings.PRESIDIO_ANONYMIZER_SERVER}:{settings.PRESIDIO_ANONYMIZER_PORT}/anonymize"
+
+    async def anonymize(self, req: AnalyzerRequest) -> Sequence[AnalyzerResponseItem]:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url=f"{self.url}",
+                json=req.model_dump(exclude_none=True),
+            )
+            return analyzer_response.validate_python(response.raise_for_status().json())
