2b79a3613d951b8c7e1c11dab89bfda838d03605
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-06-11 15:41:07 +0200

    ok

--- experiments/finetuning/mistral/create_gpu_instance.py
+++ experiments/finetuning/mistral/create_gpu_instance.py
@@ -7,23 +7,46 @@ import boto3
 logging.basicConfig(level=logging.INFO)
 
 class Infra:
-    def __init__(self, name: str = 'arena', region: str = 'eu-north-1', instance_type='g5.2xlarge', image='ami-03dc624a6bfdafc83'):
+    def __init__(self, name: str = 'arena', region: str = 'eu-north-1', instance_type='g5.2xlarge', image='ami-0d47c2063be189fce'):
         self.name = name
         self.region = region
+        self.security_group_name = f'{self.name}-security-group'
         self.key_pair = f'{self.name}-key-pair'
         self.instance_name = f'{self.name}-gpu-instance'
         self.instance_type = instance_type
         self.image = image
         self.client = boto3.client('ec2', region_name=self.region)
+        self.tags = lambda name: [{'Key': 'App', 'Value': 'arena'}, {'Key': 'Name', 'Value': f'arena-{name}'}]
     
+    def security_group(self):
+        try:
+            security_group = self.client.create_security_group(
+                Description = 'The security group for Arena GPU tests',
+                GroupName = self.security_group_name,
+                TagSpecifications = [{
+                    'ResourceType': 'security-group',
+                    'Tags': self.tags('security-group'),
+                }]
+            )
+            self.security_group_id = security_group['GroupId']
+        except Exception as e:
+            security_groups = self.client.describe_security_groups(GroupNames=[self.security_group_name])
+            self.security_group_id = security_groups['SecurityGroups'][0]['GroupId']
+            logging.warning(e)
+
     def public_key(self, public_key_path: str):
         public_key_path = os.path.expanduser(public_key_path)
         with open(public_key_path, 'rb') as file:
             public_key = file.read()
+            print(public_key)
         try:
             self.client.import_key_pair(
                 KeyName=self.key_pair,
-                PublicKeyMaterial=public_key
+                PublicKeyMaterial=public_key,
+                TagSpecifications = [{
+                    'ResourceType': 'key-pair',
+                    'Tags': self.tags('key-pair'),
+                }]
             )
         except Exception as e:
             logging.warning(e.response['Error']['Message'])
@@ -38,16 +61,11 @@ class Infra:
             'KeyName': self.key_pair,  # replace with your key pair name
             'InstanceInitiatedShutdownBehavior': 'terminate',  # instance terminates on shutdown
             'TagSpecifications': [
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': self.instance_name  # Replace with your desired instance name
-                    }
-                ]
-            }
-    ]
+                {
+                    'ResourceType': 'instance',
+                    'Tags': self.tags(self.instance_name)
+                }
+            ]
         }
         # Create the instance
         try:
@@ -67,7 +85,8 @@ class Infra:
 
 if __name__ == '__main__':
     infra = Infra()
-    infra.public_key('~/.ssh/id_rsa.pub')
+    # infra.security_group()
+    infra.public_key('~/.ssh/aws.pub')
     instance = infra.gpu_instance()
     with open('instance.json', 'w') as file:
         json.dump(instance, file, indent=2, sort_keys=True, default=str)
