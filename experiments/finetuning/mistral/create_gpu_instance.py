f9ae13bccacaa4d38c74fa37348c70d036f34727
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-06-11 19:29:13 +0200

    ok

--- experiments/finetuning/mistral/create_gpu_instance.py
+++ experiments/finetuning/mistral/create_gpu_instance.py
@@ -3,11 +3,12 @@ import logging
 import time
 import json
 import boto3
+from fabric import Connection
 
 logging.basicConfig(level=logging.INFO)
 
 class Infra:
-    def __init__(self, name: str = 'arena', region: str = 'eu-north-1', instance_type='g5.2xlarge', image='ami-0d47c2063be189fce'):
+    def __init__(self, name: str = 'arena', region: str = 'eu-north-1', instance_type='g5.2xlarge', image='ami-0d47c2063be189fce', key='~/.ssh/aws'):
         self.name = name
         self.region = region
         self.security_group_name = f'{self.name}-security-group'
@@ -15,11 +16,12 @@ class Infra:
         self.instance_name = f'{self.name}-gpu-instance'
         self.instance_type = instance_type
         self.image = image
+        self.key = key
         self.client = boto3.client('ec2', region_name=self.region)
         self.tags = lambda name: [{'Key': 'App', 'Value': 'arena'}, {'Key': 'Name', 'Value': f'arena-{name}'}]
         self.security_group()
         self.authorize_ssh()
-        self.public_key('~/.ssh/aws.pub')
+        self.public_key()
     
     def security_group(self):
         """Create a Security Group if not exist"""
@@ -59,8 +61,8 @@ class Infra:
         except Exception as e:
             logging.warning(e)
 
-    def public_key(self, public_key_path: str):
-        public_key_path = os.path.expanduser(public_key_path)
+    def public_key(self):
+        public_key_path = os.path.expanduser(f'{self.key}.pub')
         with open(public_key_path, 'rb') as file:
             public_key = file.read()
             print(public_key)
@@ -96,22 +98,32 @@ class Infra:
         # Create the instance
         try:
             instances = self.client.run_instances(**instance_params)
-            instance = instances['Instances'][0]
-            logging.info(f"Created instance: {instance['InstanceId']}")
-            while 'PublicIpAddress' not in instance:
-                logging.info('Instance:', instance)
+            self.instance = instances['Instances'][0]
+            logging.info(f"Created instance: {self.instance['InstanceId']}")
+            while 'PublicIpAddress' not in self.instance:
+                logging.info('Instance:', self.instance)
                 time.sleep(1)
-                instances = self.client.describe_instances(InstanceIds=[instance['InstanceId']])
-                instance = instances['Reservations'][0]['Instances'][0]
-            return instance
+                instances = self.client.describe_instances(InstanceIds=[self.instance['InstanceId']])
+                self.instance = instances['Reservations'][0]['Instances'][0]
+            return self.instance
         except Exception as e:
             logging.error(e)
-
+    
+    def push_git_key(self, private_key_path: str):
+        private_key_path = os.path.expanduser(private_key_path)
+        Connection(
+            host=self.instance['PublicDnsName'],
+            user='ubuntu',
+            connect_kwargs={
+                'key_filename': os.path.expanduser(private_key_path),
+            },
+            ).put(private_key_path, remote='/home/ubuntu/.ssh/id_rsa')
     
 
 if __name__ == '__main__':
     infra = Infra()
     instance = infra.gpu_instance()
+    infra.push_git_key('~/.ssh/id_rsa')
     with open('instance.json', 'w') as file:
         json.dump(instance, file, indent=2, sort_keys=True, default=str)
     print(f"{instance['PublicDnsName']} ({instance['PublicIpAddress']})")
