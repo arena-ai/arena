a19db927a73b5be3fd32f64b867b72fb984c82a0
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-09-16 14:18:15 +0200

    ok

--- backend/app/api/routes/documents.py
+++ backend/app/api/routes/documents.py
@@ -54,25 +54,32 @@ async def read_file(*, current_user: CurrentUser, name: str):
 
 
 @router.get("/{name}/as_text")
-async def read_file_as_text(*, current_user: CurrentUser, name: str):
+async def read_file_as_text(*, current_user: CurrentUser, name: str, start_page: int = 0, end_page: int | None = None):
     user_id = current_user.id
     docs = Documents()
     path = get_path(docs, current_user, name)
     input = BytesIO(docs.get(f"{path}data").read())
     content_type = docs.gets(f"{path}content_type")
-    if not docs.exists(f"{path}as_text"):
+    if end_page:
+        path_as_text = f"{path}as_text_from_page_{start_page}_to_{end_page}"
+    elif start_page>0:
+        path_as_text = f"{path}as_text_from_page_{start_page}"
+    else:
+        path_as_text = f"{path}as_text"
+    if not docs.exists(path_as_text):
         # The doc should be created
         if content_type=='application/pdf':
             doc = pymupdf.Document(stream=input)
             output = BytesIO()
-            for page in doc: # iterate the document pages
+            pages = [page for page_num, page in enumerate(doc) if page_num>=start_page and (not end_page or page_num<end_page)]
+            for page in pages: # iterate the document pages
                 text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
                 output.write(text) # write text of page
                 output.write(bytes((12,))) # write page delimiter (form feed 0x0C)
             output.seek(0)
-            docs.put(f"{path}as_text", output)
+            docs.put(path_as_text, output)
         else:
-            docs.puts(f"{path}as_text", "Error: Could not read as text")
+            docs.puts(path_as_text, "Error: Could not read as text")
     # output the file
-    input = docs.get(f"{path}as_text")
+    input = docs.get(path_as_text)
     return StreamingResponse(content=input.stream(), media_type='text/plain')
