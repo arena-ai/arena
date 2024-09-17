a254b7c55d5b20e63f423421fb513aa2ac62e01b
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-09-17 22:32:03 +0200

    ok

--- backend/app/tests/ops/test_object_store_ops.py
+++ backend/app/tests/ops/test_object_store_ops.py
@@ -1,16 +1,20 @@
 import io
 from anyio import run
+from sqlmodel import Session
 
-from app.models import UserOut
+from app import crud
+from app.models import UserCreate
+from app.ops.session import session, user
 from app.ops.documents import Paths
+from app.tests.utils.utils import random_email, random_lower_string
 
 
-def test_paths() -> None:
+def test_paths(db: Session) -> None:
     paths = Paths()
-    run(paths(UserOut(      
-        email='admin@example.com',
-        is_active=True,
-        is_superuser=True,
-        password='foo',
-        id=1,
-        )))
+    ses = session()
+    random_user = crud.create_user(session=db, user_create=UserCreate(email=random_email(), password=random_lower_string()))
+    usr = user(ses, random_user.id)
+    val = paths(usr)
+    async def test_eval():
+        return await val.evaluate(session=db)
+    print(run(test_eval))
