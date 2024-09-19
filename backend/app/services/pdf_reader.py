0180f095feb45a0a3b8373f80aa579926dde4149
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2024-09-19 17:51:13 +0200

    ok

--- backend/app/services/pdf_reader.py
+++ backend/app/services/pdf_reader.py
@@ -3,18 +3,18 @@ from dataclasses import dataclass
 import pymupdf
 from io import StringIO
 
-
+# TODO Marta / Luca: I let you make this better. The signature of the function should be unchanged.
 
 @dataclass
 class PDFReader:
-    sorted: bool = True
+    sort: bool = True
 
     def as_text(self, pdf_data: BinaryIO, start_page: int = 0, end_page: int | None = None) -> str:
         doc = pymupdf.Document(stream=pdf_data)
         output = StringIO()
         pages = [page for page_num, page in enumerate(doc) if page_num>=start_page and (not end_page or page_num<end_page)]
         for page in pages: # iterate the document pages
-            text = page.get_text(sorted=self.sorted)
+            text = page.get_text(sort=self.sort)
             output.write(f"{text}\f") # write text of page
         return output.getvalue()
 
